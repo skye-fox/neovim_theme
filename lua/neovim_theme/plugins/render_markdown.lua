8b1a38bc26441cf2f9fd817ab945aaef4769835c
SkyeFox <105951568+skye-fox@users.noreply.github.com>
2025-10-01 19:29:24 -0500

    Update render-markdown highlights

--- lua/neovim_theme/plugins/render_markdown.lua
+++ lua/neovim_theme/plugins/render_markdown.lua
@@ -3,14 +3,32 @@ local M = {}
 function M.setup(pal)
 	local set = vim.api.nvim_set_hl
 
-	set(0, "RenderMarkdownH1Bg", { bg = pal.purple })
-	set(0, "RenderMarkdownH2Bg", { bg = pal.peach })
-	set(0, "RenderMarkdownH3Bg", { bg = pal.lavender })
-	set(0, "RenderMarkdownH4Bg", { bg = pal.light_blue })
-	set(0, "RenderMarkdownH5Bg", { bg = pal.pink })
-	set(0, "RenderMarkdownH6Bg", { bg = pal.yellow })
+	-- Heading background colors
+	set(0, "RenderMarkdownH1Bg", { bg = pal.purple, fg = pal.bg02, bold = true })
+	set(0, "RenderMarkdownH2Bg", { bg = pal.peach, fg = pal.bg02, bold = true })
+	set(0, "RenderMarkdownH3Bg", { bg = pal.lavender, fg = pal.bg02, bold = true })
+	set(0, "RenderMarkdownH4Bg", { bg = pal.light_blue, fg = pal.bg02, bold = true })
+	set(0, "RenderMarkdownH5Bg", { bg = pal.pink, fg = pal.bg02, bold = true })
+	set(0, "RenderMarkdownH6Bg", { bg = pal.yellow, fg = pal.bg02, bold = true })
+
+	-- Heading text colors
+	set(0, "RenderMarkdownH1", { fg = pal.bg02 })
+	set(0, "RenderMarkdownH2", { bg = pal.bg02 })
+	set(0, "RenderMarkdownH3", { bg = pal.bg02 })
+	set(0, "RenderMarkdownH4", { bg = pal.bg02 })
+	set(0, "RenderMarkdownH5", { bg = pal.bg02 })
+	set(0, "RenderMarkdownH6", { bg = pal.bg02 })
+
+	-- Quote colors
 	set(0, "RenderMarkdownQuote", { fg = pal.peach })
+	set(0, "RenderMarkdownQuote4", { fg = pal.peach })
+
+	-- Table colors
 	set(0, "RenderMarkdownTableRow", { fg = pal.purple })
+
+	-- Task colors
+	set(0, "RenderMarkdownChecked", { fg = pal.spring })
+	set(0, "RenderMarkdownUnchecked", { fg = pal.spring })
 end
 
 return M
